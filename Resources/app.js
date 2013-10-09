58e31c2e0000843c6678cab1f630bc70df2ee0ed
Md Mostafizur Rahman <csemrm@gmail.com>
2013-10-09 20:54:54 +0600

    code added

--- Resources/app.js
+++ Resources/app.js
@@ -1,44 +1,70 @@
-/*
- * Single Window Application Template:
- * A basic starting point for your application.  Mostly a blank canvas.
- * 
- * In app.js, we generally take care of a few things:
- * - Bootstrap the application with any data we need
- * - Check for dependencies like device type, platform version or network connection
- * - Require and open our top-level UI component
- *  
- */
-
-//bootstrap and check dependencies
-if (Ti.version < 1.8 ) {
-	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
-}
-
-// This is a single context application with multiple windows in a stack
 (function() {
-	//render appropriate components based on the platform and form factor
-	var osname = Ti.Platform.osname,
-		version = Ti.Platform.version,
-		height = Ti.Platform.displayCaps.platformHeight,
-		width = Ti.Platform.displayCaps.platformWidth;
+
+	var form_url = '/upload.php';
+	var win = Ti.UI.createWindow({
+		layout : 'vertical',
+		backgroundColor : '#fff'
+	});
 	
-	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
-	//yourself what you consider a tablet form factor for android
-	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
-	
-	var Window;
-	if (isTablet) {
-		Window = require('ui/tablet/ApplicationWindow');
-	}
-	else {
-		// Android uses platform-specific properties to create windows.
-		// All other platforms follow a similar UI pattern.
-		if (osname === 'android') {
-			Window = require('ui/handheld/android/ApplicationWindow');
-		}
-		else {
-			Window = require('ui/handheld/ApplicationWindow');
-		}
-	}
-	new Window().open();
+	var btnPhoto = Ti.UI.createButton({
+		title : 'Select Photo'
+	});
+	btnPhoto.addEventListener('click', function(e) {
+		// Open photo gallery for user to select photo
+		Ti.Media.openPhotoGallery({
+			mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO],
+			success : function(e) {
+				btnPhoto.value = e.media;
+				anImageView.image = e.media;
+				btnPhoto.title = '[ Photo Selected ]';
+			},
+			cancel : function() {
+				btnPhoto.value = null;
+				btnPhoto.title = 'Select Photo...';
+			},
+			error : function(err) {
+				Ti.API.error(err);
+				btnPhoto.value = null;
+				btnPhoto.title = 'Select Photo...';
+			}
+		});
+	});
+	win.add(btnPhoto);
+
+	var btnSubmit = Ti.UI.createButton({
+		title : 'Submit'
+	});
+	btnSubmit.addEventListener('click', function(e) {
+
+		var c = Titanium.Network.createHTTPClient({
+			onload : function(e) {
+
+				Ti.API.info('this.responseText' + this.responseText);
+				json = JSON.stringify(this.responseText);
+				//alert(this.responseText);
+			},
+			onerror : function(e) {
+				//alert(JSON.stringify(e));
+			}
+		});
+		c.open('POST', form_url);
+		Ti.API.info('form_url ' + encodeURI(form_url));
+	//	c.setRequestHeader('Content-Type', 'multipart/form-data');
+		c.send({
+			userid : 81,
+			image : btnPhoto.value
+		});
+	});
+	win.add(btnSubmit);
+	var anImageView = Ti.UI.createImageView({
+		 
+	});
+	anImageView.addEventListener('load', function() {
+		Ti.API.info('Image loaded!');
+	});
+
+	// Add to the parent view.
+	win.add(anImageView);
+	win.open();
+
 })();
